822fc9a7582d0357d2f50afbe76e78da2d640ef4
remymuhire <jeanremy1806@gmail.com>
2017-08-07 12:39:07 +0200

    modified

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import os
 
-from app import create_app
+from app import create_app, db
+from app.models import User
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
+    with app.app_context():
+        # create a development user
+        if User.query.get(1) is None:
+            u = User(username='rmuhire')
+            u.set_password('18061992')
+            db.session.add(u)
+            db.session.commit()
     app.run()
