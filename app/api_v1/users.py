dd0f69cab13331dda4fab24e0db77da4474b14f7
remymuhire <jeanremy1806@gmail.com>
2017-08-08 17:12:10 +0200

    bug fixed no-cached

--- app/api_v1/users.py
+++ app/api_v1/users.py
@@ -1,8 +1,30 @@
+from flask import request
 from . import api
-from flask import jsonify, request
+from .. import db
+from ..models import User, Organization
+from ..decorators import json, paginate, no_cache
 
 
 @api.route('/users/', methods=['GET'])
+@no_cache
+@json
+@paginate('users')
 def get_users():
-    return jsonify({'user':'Remyoooo'})
+    return User.query
+
+
+@api.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    return User.query.get_or_404(id)
+
+
+@api.route('/organization/<int:id>/users/', methods=['POST'])
+def new_user(id):
+    organization = Organization.query.get_or_404(id)
+    user = User(organization=organization)
+    user.import_data(request.json)
+    user.set_password(request.json['password'])
+    db.session.add(user)
+    db.session.commit()
+    return {}, 201, {'Location': user.get_url()}
 
