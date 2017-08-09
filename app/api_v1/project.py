4f5eb2e7f724b602523515a67f46c3fb8cba2f8b
remymuhire <jeanremy1806@gmail.com>
2017-08-09 15:54:32 +0200

    project api

---
+++ app/api_v1/project.py
@@ -0,0 +1,40 @@
+from flask import request
+from . import api
+from .. import db
+from ..models import Project, Organization
+from ..decorators import json, paginate, no_cache
+
+
+@api.route('/projects/<int:id>', methods=['GET'])
+@json
+def get_project(id):
+    return Project.query.get_or_404(id)
+
+
+@api.route('/organizations/<int:id>/projects/', methods=['GET'])
+@json
+@paginate('projects')
+def get_organization_project(id):
+    organization = Organization.query.get_or_404(id)
+    return organization.project
+
+
+@api.route('/organizations/<int:id>/projects/', methods=['POST'])
+@json
+def new_project(id):
+    organization = Organization.query.get_or_404(id)
+    project = Project(organization=organization)
+    project.import_data(request.json)
+    db.session.add(project)
+    db.session.commit()
+    return {}, 201, {'Location': project.get_url()}
+
+
+@api.route('/projects/<int:id>', methods=['PUT'])
+@json
+def edit_project(id):
+    project = Project.query.get_or_404(id)
+    project.import_data(request.json)
+    db.session.add(project)
+    db.session.commit()
+    return {}
