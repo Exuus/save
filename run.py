e4253de14c8d327580ef48681a48d81dc9a4d244
remymuhire <jeanremy1806@gmail.com>
2017-08-11 18:21:25 +0200

    run py

--- run.py
+++ run.py
@@ -2,9 +2,18 @@ import os
 
 from app import create_app, db
 from app.models import User
-
+from flask import jsonify
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
+
+
+@app.route('/user/')
+def new_user():
+    u = User(username='rmuhire')
+    u.set_password('18061992')
+    db.session.add(u)
+    db.session.commit()
+    return jsonify(True)
 
 if __name__ == '__main__':
     with app.app_context():
