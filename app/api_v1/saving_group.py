370c50ca67b3d50306a9828637ce7ded69cac65b
remymuhire <jeanremy1806@gmail.com>
2017-09-12 17:31:31 +0200

    saving group api

--- app/api_v1/saving_group.py
+++ app/api_v1/saving_group.py
@@ -4,9 +4,8 @@ from .. import db
 from ..models import SavingGroup, SavingGroupCycle, SavingGroupDropOut,\
     SavingGroupFinDetails, SavingGroupMember, SavingGroupWallet, \
     MemberLoan, MemberSocialFund, MemberApprovedSocial, MemberApprovedLoan,\
-    SgMemberContributions, Project, ProjectAgent, Organization
+    SgMemberContributions, Project, ProjectAgent, Organization, and_, datetime
 from ..decorators import json, paginate, no_cache
-from sqlalchemy import and_
 from sqlalchemy.exc import IntegrityError
 
 
@@ -181,29 +180,99 @@ def get_member_pending_loan(id):
     return {}, 404,
 
 
+@api.route('/approved/loan/<int:id>', methods=['GET'])
+@json
+def get_approved_loan(id):
+    return MemberApprovedLoan.query.get_or_404(id)
+
+
+@api.route('/member/admin/<int:id>/loan/pending/', methods=['GET'])
+@json
+@paginate('pending_loan')
+def get_admin_pending_loan(id):
+    member = SavingGroupMember.query.\
+        filter(and_(SavingGroupMember.admin == 1, SavingGroupMember.id == id)).\
+        first()
+    if member:
+        return member.approved_loan
+    return {}, 404
+
+
 @api.route('/member/<int:id>/loan/', methods=['POST'])
 @json
 def new_loan_request(id):
     member = SavingGroupMember.query.get_or_404(id)
+    admins = SavingGroupMember.group_admin(member.saving_group_id)
+
     if member:
-        if member.verify_pin(request.json['pin']):
-            wallet = SavingGroupWallet.query. \
-                filter(SavingGroupWallet.saving_group_id == member.saving_group_id).first()
-            cycle = SavingGroupCycle.query. \
-                filter(and_(SavingGroupCycle.active == 1,
-                            SavingGroupCycle.saving_group_id == member.saving_group_id)). \
-                first()
-            loan = MemberLoan(
-                sg_cycle=cycle,
-                sg_wallet=wallet,
-                sg_member=member
-            )
+        if len(admins.all()):
+            if member.verify_pin(request.json['pin']):
+                wallet = SavingGroupWallet.query. \
+                    filter(SavingGroupWallet.saving_group_id == member.saving_group_id).first()
+                cycle = SavingGroupCycle.query. \
+                    filter(and_(SavingGroupCycle.active == 1,
+                                SavingGroupCycle.saving_group_id == member.saving_group_id)). \
+                    first()
+                loan = MemberLoan(
+                    sg_cycle=cycle,
+                    sg_wallet=wallet,
+                    sg_member=member
+                )
 
-            loan.import_data(request.json)
-            db.session.add(loan)
-            db.session.commit()
-            return {}, 201, {'Location': loan.get_url()}
+                loan.import_data(request.json)
+                db.session.add(loan)
+                db.session.commit()
 
+                """ add member approve Request """
+
+                for admin in admins:
+                    data = dict()
+                    data['status'] = 2
+                    data['sg_member_id'] = admin.export_data()['id']
+                    approved_loan = MemberApprovedLoan(member_loan=loan)
+                    approved_loan.import_data(data)
+                    db.session.add(approved_loan)
+                    db.session.commit()
+                return {}, 201, {'Location': loan.get_url()}
+
+    return {}, 404
+
+
+@api.route('/member/admin/<int:member_id>/approve/loan/<int:id>/', methods=['PUT'])
+@json
+def approve_loan(member_id, id):
+    member = SavingGroupMember.query.\
+        filter(and_(SavingGroupMember.admin == 1, SavingGroupMember.id == member_id)).\
+        first()
+    if member:
+        try:
+            if member.verify_pin(request.json['pin']):
+                approved_loan = MemberApprovedLoan.query.get_or_404(id)
+                approved_loan.approve_loan()
+                db.session.add(approved_loan)
+                db.session.commit()
+                return {}, 200
+        except AttributeError:
+            return {}, 404
+    return {}, 404
+
+
+@api.route('/member/admin/<int:member_id>/decline/loan/<int:id>/', methods=['PUT'])
+@json
+def decline_loan(member_id, id):
+    member = SavingGroupMember.query.\
+        filter(and_(SavingGroupMember.admin == 1, SavingGroupMember.id == member_id)).\
+        first()
+    if member:
+        try:
+            if member.verify_pin(request.json['pin']):
+                approved_loan = MemberApprovedLoan.query.get_or_404(id)
+                approved_loan.decline_loan()
+                db.session.add(approved_loan)
+                db.session.commit()
+                return {}, 200
+        except AttributeError:
+            return {}, 404
     return {}, 404
 
 
