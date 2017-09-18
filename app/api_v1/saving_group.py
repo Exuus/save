435d3601c69a686f6ccbf5716d796d6ab40d1e53
remymuhire <jeanremy1806@gmail.com>
2017-09-18 19:55:18 +0200

    social fund api

--- app/api_v1/saving_group.py
+++ app/api_v1/saving_group.py
@@ -279,6 +279,39 @@ def get_social_fund(id):
     return MemberSocialFund.query.get_or_404(id)
 
 
+@api.route('/approve/social-fund/<int:id>')
+@json
+def get_approve_social_fund(id):
+    return MemberApprovedSocial.query.get_or_404(id)
+
+
+@api.route('/members/<int:id>/social-fund/pending/', methods=['GET'])
+@json
+@paginate('member_social_fund')
+def get_member_pending_social_fund(id):
+    member = SavingGroupMember.query.get_or_404(id)
+    if member:
+        return MemberSocialFund.query\
+            .filter(MemberSocialFund.sg_member_id == member.id)\
+            .join(SavingGroupCycle)\
+            .filter(SavingGroupCycle.saving_group_id == member.saving_group_id)\
+            .join(MemberApprovedSocial)\
+            .filter(MemberApprovedSocial.status == 2)
+    return {}, 404,
+
+
+@api.route('/members/admin/<int:id>/social-fund/pending/', methods=['GET'])
+@json
+@paginate('pending_social_fund')
+def get_admin_pending_social(id):
+    member = SavingGroupMember.query.\
+        filter(and_(SavingGroupMember.admin == 1, SavingGroupMember.id == id)).\
+        first()
+    if member:
+        return member.approve_social
+    return {}, 404
+
+
 @api.route('/members/<int:id>/social-fund/', methods=['POST'])
 @json
 def member_social_fund_request(id):
@@ -316,6 +349,44 @@ def member_social_fund_request(id):
                     db.session.commit()
                 return {}, 201, {'Location': social_fund.get_url()}
 
+    return {}, 404
+
+
+@api.route('/members/admin/<int:member_id>/approve/social-fund/<int:id>/', methods=['PUT'])
+@json
+def approve_social(member_id, id):
+    member = SavingGroupMember.query.\
+        filter(and_(SavingGroupMember.admin == 1, SavingGroupMember.id == member_id)).\
+        first()
+    if member:
+        try:
+            if member.verify_pin(request.json['pin']):
+                approved_social = MemberApprovedSocial.query.get_or_404(id)
+                approved_social.approve_social()
+                db.session.add(approved_social)
+                db.session.commit()
+                return {}, 200
+        except AttributeError:
+            return {}, 404
+    return {}, 404
+
+
+@api.route('/members/admin/<int:member_id>/decline/social-fund/<int:id>/', methods=['PUT'])
+@json
+def decline_social(member_id, id):
+    member = SavingGroupMember.query.\
+        filter(and_(SavingGroupMember.admin == 1, SavingGroupMember.id == member_id)).\
+        first()
+    if member:
+        try:
+            if member.verify_pin(request.json['pin']):
+                approved_social = MemberApprovedSocial.query.get_or_404(id)
+                approved_social.decline_social()
+                db.session.add(approved_social)
+                db.session.commit()
+                return {}, 200
+        except AttributeError:
+            return {}, 404
     return {}, 404
 
 
