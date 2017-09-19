bae78ceaa2a90ffc2618d7f39f3929a4350f35e0
remymuhire <jeanremy1806@gmail.com>
2017-09-19 15:52:44 +0200

    wallet api

--- app/api_v1/sg/wallet.py
+++ app/api_v1/sg/wallet.py
@@ -1,6 +1,6 @@
 from .. import api
 from ...models import SavingGroupWallet, SavingGroup, MemberLoan, SavingGroupCycle, \
-    MemberApprovedLoan
+    MemberApprovedLoan, MemberApprovedSocial, MemberSocialFund, db
 from ...decorators import json, paginate, no_cache
 
 
@@ -17,11 +17,20 @@ def get_wallet(id):
 @paginate('wallet')
 def get_sg_wallet(id):
     saving_group = SavingGroup.query.get_or_404(id)
-    import pdb; pdb.set_trace()
     return saving_group.sg_wallet
 
 
-@api.route('/wallet/<int:id>/loan/')
+@api.route('/sg/<int:id>/wallet/', methods=['POST'])
+@json
+def new_sg_wallet(id):
+    saving_group = SavingGroup.query.get_or_404(id)
+    sg_wallet = SavingGroupWallet(saving_group=saving_group)
+    db.session.add(sg_wallet)
+    db.session.commit()
+    return {}, 201, {'Location': sg_wallet.get_url()}
+
+
+@api.route('/wallet/<int:id>/loan/', methods=['GET'])
 @no_cache
 @json
 @paginate('loan')
@@ -34,6 +43,44 @@ def get_wallet_loan(id):
             .filter(SavingGroupCycle.saving_group_id == wallet.saving_group_id) \
             .join(MemberApprovedLoan) \
             .filter(MemberApprovedLoan.status == 1)
-    return {}, 404,
+    return {}, 404
+
+
+@api.route('/wallet/<int:id>/social-fund/debit/', methods=['GET'])
+@no_cache
+@json
+@paginate('social_fund_debit')
+def get_wallet_social_fund_debit(id):
+    wallet = SavingGroupWallet.query.get_or_404(id)
+    if wallet:
+        return MemberSocialFund.query \
+            .filter(MemberSocialFund.sg_wallet_id == wallet.id) \
+            .join(SavingGroupCycle) \
+            .filter(SavingGroupCycle.saving_group_id == wallet.saving_group_id) \
+            .join(MemberApprovedSocial) \
+            .filter(MemberApprovedSocial.status == 1)
+    return {}, 404
+
+
+@api.route('/wallet/<int:id>/social-fund/credit/', methods=['GET'])
+@no_cache
+@json
+@paginate('social_fund_credit')
+def get_wallet_social_fund_credit(id):
+    wallet = SavingGroupWallet.query.get_or_404(id)
+    if wallet:
+        return wallet.member_transaction.filter_by(type=2)
+    return {}, 404
+
+
+@api.route('/wallet/<int:id>/savings/', methods=['GET'])
+@no_cache
+@json
+@paginate('savings')
+def get_wallet_savings(id):
+    wallet = SavingGroupWallet.query.get_or_404(id)
+    if wallet:
+        return wallet.member_transaction.filter_by(type=1)
+    return {}, 404
 
 
