dd90d4e65593b1ad3d4cbad5614a21ca1934e08c
remymuhire <jeanremy1806@gmail.com>
2017-10-06 13:03:34 +0200

    sg_ministatement

--- app/api_v1/sg/member/ministatement.py
+++ app/api_v1/sg/member/ministatement.py
@@ -1,5 +1,5 @@
 from ... import api
-from ....models import SavingGroupMember, MemberMiniStatement
+from ....models import SavingGroupMember, MemberMiniStatement, SavingGroup, and_
 from ....decorators import json, paginate, no_cache
 
 
@@ -12,8 +12,21 @@ def get_mini_statement(id):
 @api.route('/members/<int:id>/mini-statement/')
 @no_cache
 @json
-@paginate('mini_statement')
+@paginate('member_mini_statement')
 def get_members_mini_statement(id):
     member = SavingGroupMember.query.get_or_404(id)
     return member.member_mini_statement
+
+
+@api.route('/sg/<int:id>/mini-statement/')
+@no_cache
+@json
+@paginate('sg_mini_statement')
+def get_sg_mini_statement(id):
+    saving_group = SavingGroup.query.get_or_404(id)
+    return MemberMiniStatement.query.\
+        join(SavingGroupMember, SavingGroup).\
+        filter(and_(SavingGroupMember.id == MemberMiniStatement.member_id,
+                    SavingGroupMember.saving_group_id == SavingGroup.id,
+                    SavingGroup.id == saving_group.id))
 
