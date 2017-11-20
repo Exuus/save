91c2ba7efaf0fd5bc7052e3b4c51796dfe38914e
remymuhire <jeanremy1806@gmail.com>
2017-11-20 22:00:06 +0200

    ini

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -6,7 +6,7 @@ api = Blueprint('api', __name__)
 
 
 @api.before_request
-@rate_limit(limit=15, period=15)
+@rate_limit(limit=150, period=1)
 @auth_token.login_required
 def before_request():
     """All routes in this blueprint require authentication."""
