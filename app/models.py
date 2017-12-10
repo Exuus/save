de4832c8c8153e504358b1f0a1ee88b14238b774
remymuhire <jeanremy1806@gmail.com>
2017-12-10 21:11:33 +0200

    models py

--- app/models.py
+++ app/models.py
@@ -73,6 +73,7 @@ class User(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow())
     birth_date = db.Column(db.Date)
     gender = db.Column(db.Integer)  # 0 Male # 1 Female
+    key = db.Column(db.String(64))
     education = db.Column(db.String(64), nullable=True)
     id_number = db.Column(db.String(60), unique=True, nullable=True)
     location = db.Column(db.String(128), default='no location')
@@ -94,6 +95,14 @@ class User(db.Model):
     def generate_auth_token(self):
         s = Serializer(current_app.config['SECRET_KEY'])
         return s.dumps({'id': self.id}).decode('utf-8')
+
+    def reset_key(self):
+        self.key = None
+        return self
+
+    def generate_sms_key(self):
+        self.key = generate_code()
+        return self.key
 
     def get_url(self):
         return url_for('api.get_user', id=self.id, _external=True)
