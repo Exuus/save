591576849e043a4b01db3356f9959b61c29db1da
remymuhire <jeanremy1806@gmail.com>
2017-12-14 11:45:44 +0200

    kenessa fixed

--- app/api_v1/kenessa.py
+++ app/api_v1/kenessa.py
@@ -12,22 +12,26 @@ def get_kenessa_province():
 @api.route('/kenessa/<province_id>/districts/')
 @json
 def get_kenessa_district(province_id):
+    province_id = province_id.split(',')
     return kenessa.get_district(province_id)
 
 
 @api.route('/kenessa/<district_id>/sectors/')
 @json
 def get_kenessa_sector(district_id):
+    district_id = district_id.split(',')
     return kenessa.get_sector(district_id)
 
 
 @api.route('/kenessa/<sector_id>/cells/')
 @json
 def get_kenessa_cell(sector_id):
+    sector_id = sector_id.split(',')
     return kenessa.get_cell(sector_id)
 
 
 @api.route('/kenessa/<cell_id>/villages/')
 @json
 def get_kenessa_village(cell_id):
+    cell_id = cell_id.split(',')
     return kenessa.get_village(cell_id)
