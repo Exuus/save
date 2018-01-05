0a285e0a9eec179bd3c96b292a49409211c7027b
remymuhire <jeanremy1806@gmail.com>
2018-01-05 19:04:32 +0200

    fine

--- app/api_v1/sg/member/fine.py
+++ app/api_v1/sg/member/fine.py
@@ -2,7 +2,7 @@ from flask import request
 from ... import api
 from .... import db
 from ....models import SavingGroupCycle, MemberFine, \
-    SavingGroupMember, and_, SavingGroupWallet, SavingGroupFines
+    SavingGroupMember, and_, SavingGroupWallet, SavingGroupFines, MemberFineRepayment
 from ....decorators import json, paginate, no_cache
 
 
@@ -24,6 +24,12 @@ def get_member_fine(id):
         json['fine'] = fine[0]
         json['name'] = fine[1]
         json['acronym'] = fine[2]
+        json['id'] = fine[3]
+        json['payed'] = 0 if MemberFineRepayment.fine_balance(json['id']) is None \
+            else MemberFineRepayment.fine_balance(json['id'])
+        json['date'] = fine[4]
+        json['fine_balance'] = int(json['fine']) - int(json['payed'])
+        json['status'] = fine[5]
         data.append(json)
 
     return data
@@ -54,5 +60,40 @@ def new_member_fine(id, fine_id):
         db.session.commit()
         return {}, 201, {'Location': member_fine.get_url()}
     return {}, 404
+
+
+@api.route('/fine-repayment/<fine_id>/', methods=['POST'])
+@json
+def new_fine_repayment(id):
+    member = SavingGroupMember.query.get_or_404(request.json['member_id'])
+    if member.verify_pin(request.json['pin']):
+        member_fine = MemberFine.query.get_or_404(id)
+        sg_fines = SavingGroupFines.query.get_or_404(member_fine.sg_fine_id)
+        payed = 0 if MemberFineRepayment.fine_balance(member_fine.id) is None \
+            else MemberFineRepayment.fine_balance(member_fine.id)
+        wallet = SavingGroupWallet.query.get_or_404(member_fine.wallet_id)
+
+        if payed < sg_fines.amount:
+            member_fine_repay = MemberFineRepayment(member_fine=member_fine)
+            member_fine_repay.import_data(request.json)
+            wallet.credit_wallet(request.json['amount'])
+            db.session.add(wallet)
+            db.session.add(member_fine_repay)
+            db.session.commit()
+
+            amount_payed = 0 if MemberFineRepayment.fine_balance(member_fine.id) is None \
+                else MemberFineRepayment.fine_balance(member_fine.id)
+
+            if amount_payed == sg_fines:
+                member_fine.repay_fine()
+                db.session.add(member_fine)
+                db.session.commit()
+
+            return {}, 201
+    return {}, 404
+
+
+
+
 
 
