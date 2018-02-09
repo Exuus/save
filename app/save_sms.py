5eeea9c07cdd5d4c2df47b7eb25e86d6bde1c7e0
remymuhire <jeanremy1806@gmail.com>
2018-02-09 14:35:08 +0200

    sms template added

--- app/save_sms.py
+++ app/save_sms.py
@@ -4,8 +4,18 @@ client = nexmo.Client(key='de087b65', secret='9c629de08d20b047')
 
 
 def save_sms(to, text):
-    client.send_message({
+    return client.send_message({
       'from': 'Save',
       'to': to,
       'text': text
     })
+
+
+def new_member_sms(sg_name, user_name, to):
+    text = user_name + '+Welcome+to+Save.You+have+been+added+to' + sg_name + '+group'
+    return client.send_message({
+        'from': 'Save',
+        'to': to,
+        'text': text
+    })
+
